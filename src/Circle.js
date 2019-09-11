7f82754e5f377e1558f3761362972aae775556ea
Master <git@github.com:emmolka/NavBar.git>
2019-09-11 21:35:55 +0200

    some-styles

--- src/Circle.js
+++ src/Circle.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from "react";
 import * as d3 from "d3";
+import "./App.css";
+import { IoIosAdd, IoIosRemove } from "react-icons/io";
 const DetailsTop = React.memo(props => {
   const [nodes, setNodes] = useState(
-    d3.range(50).map(function(d) {
+    d3.range(5).map(function(d) {
       return { radius: Math.random() * 25 + 10 };
     })
   );
+  const [gravity, setGravity] = useState("5");
 
   var width = 400,
     height = 400;
 
-  console.log(nodes);
-
-  function Mati() {
+  function draw() {
     var simulation = d3
       .forceSimulation(nodes)
-      .force("charge", d3.forceManyBody().strength(5))
+      .force("charge", d3.forceManyBody().strength(gravity))
       .force("center", d3.forceCenter(width / 2, height / 2))
       .force(
         "collision",
@@ -27,7 +28,7 @@ const DetailsTop = React.memo(props => {
 
     function ticked() {
       var u = d3
-        .select("svg")
+        .select(".svg")
         .selectAll("circle")
         .data(nodes);
 
@@ -42,25 +43,13 @@ const DetailsTop = React.memo(props => {
         })
         .attr("cy", function(d) {
           return d.y;
-        });
-
-      u.exit().remove();
-      u.enter()
-        .append("text")
-        .attr("r", function(d) {
-          return d.radius;
         })
-        .merge(u)
-        .attr("cx", function(d) {
-          return d.x;
-        })
-        .attr("cy", function(d) {
-          return d.y;
-        });
+        .attr("fill", "white");
 
       u.exit().remove();
     }
   }
+  draw();
 
   // const simulation = d3
   //   .forceSimulation(circles)
@@ -76,41 +65,40 @@ const DetailsTop = React.memo(props => {
   // const circles = [];
 
   return (
-    <div
-      className="box"
-      style={{
-        display: "flex",
-        "align-items": "center",
-        "flex-direction": "column"
-      }}
-    >
-      <div id="content">
-        <svg width="400" height="400"></svg>
+    <div className="box">
+      <div className="wrapper">
+        <IoIosAdd
+          className="icon"
+          onClick={() => {
+            nodes.push({ radius: Math.random() * 25 + 10 });
+            setNodes([...nodes]);
+            draw();
+          }}
+        ></IoIosAdd>
+        <IoIosRemove
+          className="icon"
+          onClick={() => {
+            nodes.pop();
+            setNodes([...nodes]);
+            draw();
+          }}
+        ></IoIosRemove>
       </div>
-      <button
-        onClick={() => Mati()}
-        style={{ width: 100, height: 100 }}
-      ></button>
-      <button
-        onClick={() => {
-          nodes.push({ radius: Math.random() * 25 + 10 });
-          setNodes([...nodes]);
-          Mati();
-        }}
-        style={{ width: 100, height: 100 }}
-      >
-        +
-      </button>
-      <button
-        onClick={() => {
-          nodes.pop();
-          setNodes([...nodes]);
-          Mati();
-        }}
-        style={{ width: 100, height: 100 }}
-      >
-        -
-      </button>
+      <div className="wrapper ">
+        <svg className="svg"></svg>
+      </div>
+      <div className="wrapper">
+        <input
+          type="range"
+          min="-20"
+          max="20"
+          value={gravity}
+          onChange={event => {
+            setGravity(event.target.value);
+            draw();
+          }}
+        ></input>
+      </div>
     </div>
   );
 });
